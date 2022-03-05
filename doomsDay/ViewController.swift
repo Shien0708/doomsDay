1e4cd24b93f53db12f653617cb9c3b1a3b46e342
Shien <f0928652413@gmail.com>
2022-03-05 13:18:07 +0800

    Initial Commit

---
+++ doomsDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  doomsDay
+//
+//  Created by 方仕賢 on 2022/3/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
